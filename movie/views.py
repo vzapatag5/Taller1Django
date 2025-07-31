355b5cd4603acb835e67029e74d7a9b0bdb39f8c
Valentina <vzapatag1@gmail.com>
2025-07-31 11:49:59 -0500

    cambios en la funcion about

--- movie/views.py
+++ movie/views.py
@@ -8,5 +8,5 @@ def home(request):
     return render(request, 'home.html', {'name': 'Valentina Zapata'})
 
 def about(request):
-    return HttpResponse('<h1>Welcome to About Page!</h1>')
-    
+    #return HttpResponse('<h1>Welcome to About Page!</h1>')
+     return render(request, 'about.html', {'name': 'Valentina Zapata'})
